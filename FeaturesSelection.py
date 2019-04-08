2984af421b5630723e66fceaebf804cbac754f82
Piotr Chorościn <titkot96@gmail.com>
2019-04-08 15:12:35 +0200

    Divide data into groups according to illness labels

--- FeaturesSelection.py
+++ FeaturesSelection.py
@@ -14,7 +14,7 @@ d2	decision: Nephritis of renal pelvis origin { yes, no }
 data_file = open("diagnosis01dots.csv", "r+")
 print()
 
-#results = [[], [], [], []]  # 4 classes of diseases
+groups = [[],[],[],[]]  # groups of results: 00, 01, 10, 11
 results = []
 
 for line in data_file:
@@ -24,11 +24,25 @@ for line in data_file:
     list_line = no_nl_line.split(";")
     result = [float(i) for i in list_line]
     results.append(result)
+    d1, d2 = result[-2], result[-1]
+    label = 0
+    if d1==0 and d2==0:
+        label = 0
+    elif d1==0 and d2==1:
+        label = 1
+    elif d1==1 and d2==0:
+        label = 2
+    else:
+        label = 3
+    groups[label].append(result)
     #print(result)
 
 # print(results)
 results_np = np.array(results)
 #print(results_np)
+for group in groups:
+    print(group)
+'''
 a1 = results_np[:,0]
 a2 = results_np[:,1]
 a3 = results_np[:,2]
@@ -37,7 +51,7 @@ a5 = results_np[:,4]
 a6 = results_np[:,5]
 d1 = results_np[:,6]
 d2 = results_np[:,7]
-
+'''
 # TODO divide results_np into submatricies according to one of 4 illnesses groups
 
 
