dd858cd833827bbc5e2f112fcac4bf1cf8553a8f
Piotr Chorościn <titkot96@gmail.com>
2019-04-26 18:38:26 +0200

    Change list to np.arrays to fit the model

--- FeaturesSelection.py
+++ FeaturesSelection.py
@@ -1,8 +1,15 @@
+from __future__ import absolute_import, division, print_function
 import numpy as np
 from sklearn.model_selection import train_test_split
 import Kolmogorov as kolmog
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import seaborn as sns # for data visualisation
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+
 
 '''
 a1	Temperature of patient { 35C-42C }	
@@ -32,7 +39,7 @@ for line in data_file:
     result = [float(i) for i in list_line]  # save line values as floats
 
     results.append(result)  # add line to the results
-    features.append(result[:(-2)])  # features data
+    features.append(np.array(result[:(-2)]))  # features data
 
     d1, d2 = result[-2], result[-1]  # decisions
     label = 0
@@ -46,7 +53,10 @@ for line in data_file:
         label = 3   # both nephritis and inflammation
 
     groups[label].append(result)
-    labels.append(label)
+    labels.append(np.array(label))
+
+features = np.array(features)
+labels = np.array(labels)
 
 
 # Split data into training and test sets
@@ -55,18 +65,29 @@ train, test, train_labels, test_labels = train_test_split(features,
                                                           test_size=0.33,
                                                           random_state=42)
 
-labels_occurrence = [labels.count(i) for i in range(len(label_names))]
-y_pos = np.arange(len(label_names))
+#labels_occurrence = [labels.count(i) for i in range(len(label_names))]
+#y_pos = np.arange(len(label_names))
 
+'''
 # Plot diseases occurrence
 plt.bar(y_pos, labels_occurrence, align='center', alpha=0.5)
 plt.xticks(y_pos, label_names)
 plt.ylabel('Occurrence')
 plt.title('Occurrence of diseases')
 plt.show()
-
+'''
 #   TODO normalize temperatures
 
+#   Build the NN model
+model = keras.Sequential([
+    keras.layers.Dense(5, activation=tf.nn.relu),
+    keras.layers.Dense(4, activation=tf.nn.softmax)
+])
 
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train, train_labels, epochs=5)
 
 
