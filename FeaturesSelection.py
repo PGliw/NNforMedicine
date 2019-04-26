99daa441c017299d58d6bbab293f904264047871
Piotr Chorościn <titkot96@gmail.com>
2019-04-26 17:14:14 +0200

    Make list of features data and list of labels

--- FeaturesSelection.py
+++ FeaturesSelection.py
@@ -16,56 +16,68 @@ d2	decision: Nephritis of renal pelvis origin { yes, no }
 data_file = open("diagnosis01dots.csv", "r+")
 print()
 
-groups = [[],[],[],[]]  # groups of results: 00, 01, 10, 11
+groups = [[], [], [], []]  # groups of results: 00, 01, 10, 11
 results = []
 
+features = []
+labels = []
+
 for line in data_file:
-    #print(line)
-    result = [0.0, False, False, False, False, False, False, False]
-    no_nl_line = line.rstrip("\n\r")
-    list_line = no_nl_line.split(";")
-    result = [float(i) for i in list_line]
-    results.append(result)
-    d1, d2 = result[-2], result[-1]
+    no_nl_line = line.rstrip("\n\r")  # remove new line sign
+    list_line = no_nl_line.split(";")  # split line into features
+    result = [float(i) for i in list_line]  # save line values as floats
+
+    results.append(result)  # add line to the results
+    features.append(result[:(-2)])  # features data
+
+    d1, d2 = result[-2], result[-1]  # decisions
     label = 0
-    if d1==0 and d2==0:
-        label = 0
-    elif d1==0 and d2==1:
-        label = 1
-    elif d1==1 and d2==0:
-        label = 2
+    if d1 == 0 and d2 == 0:
+        label = 0   # healthy patient
+    elif d1 == 0 and d2 == 1:
+        label = 1   # only nephritis
+    elif d1 == 1 and d2 == 0:
+        label = 2   # only inflammation
     else:
-        label = 3
+        label = 3   # both nephritis and inflammation
     groups[label].append(result)
-    #print(result)
+    labels.append(label)
 
-# print(results)
-results_np = np.array(results)
-#print(results_np)
-#for group in groups:
-#    print(group)
 
-print(groups[0])
-a100 = results_np[:,0]
-a200 = results_np[:,1]
-a300 = results_np[:,2]
-a400 = results_np[:,3]
-a500 = results_np[:,4]
-a600 = results_np[:,5]
-ys = kolmog.distribution_fun(a100)
-print(ys)
+results_np = np.array(results)  # list to np.array
 
-plt.subplot(121)
-plt.plot(a100, 'r*')
-plt.ylabel("Sample value")
-plt.xlabel("Sample no")
-plt.subplot(122)
-plt.plot(ys, 'g*')
-plt.ylabel("Distribution fun")
-plt.xlabel("Sample no")
-plt.show()
 
 '''
+def feature_distribution(xs, feature_no, group_no):
+    ys = kolmog.distribution_fun(xs)
+    plt.subplot(121)
+    plt.title("Feature {} in group {}".format(feature_no, group_no))
+    plt.plot(xs, 'r*')
+    plt.ylabel("Sample value")
+    plt.xlabel("Sample no")
+    plt.subplot(122)
+    plt.plot(ys, 'g*')
+    plt.ylabel("Distribution fun")
+    plt.xlabel("Sample no")
+    plt.show()
+
+
+#print(groups[0])
+a100 = results_np[:, 0]
+a200 = results_np[:, 1]
+a300 = results_np[:, 2]
+a400 = results_np[:, 3]
+a500 = results_np[:, 4]
+a600 = results_np[:, 5]
+
+
+feature_distribution(a100, 0, 0)
+feature_distribution(a200, 1, 0)
+feature_distribution(a300, 2, 0)
+feature_distribution(a400, 3, 0)
+feature_distribution(a500, 4, 0)
+
+
 a1 = results_np[:,0]
 a2 = results_np[:,1]
 a3 = results_np[:,2]
@@ -76,5 +88,3 @@ d1 = results_np[:,6]
 d2 = results_np[:,7]
 '''
 # TODO divide results_np into submatricies according to one of 4 illnesses groups
-
-
